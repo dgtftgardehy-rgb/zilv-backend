1deec7fccf67307da708e7a5994371d7db42c6db
dgtftgardehy-rgb <dgtftgardehy@gmail.com>
2025-12-17 09:37:36 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('后端运行中！'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
